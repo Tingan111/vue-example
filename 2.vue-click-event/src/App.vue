27c0e3fc49b9d496d06a32b1721a9e2fd3ecade3
Tingan <syeak123@gmail.com>
2025-08-02 12:59:29 +0800

    feat: feat: add click event handling example with ref

---
+++ 2.vue-click-event/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref}from 'vue'; 
+const message=ref('Hello world')
+
+function ReverseMessage(){
+  message.value=message.value.split('').reverse().join('')
+}
+</script>
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="ReverseMessage">Reverse Message</button>
+  <button @click="message+='!'">append"!"</button>
+  <a href="https://vuejs.org" @click.prevent="notify">
+    A link with e.preventDefault()
+  </a>
+</template>
+<style>
+button, a{
+
+
+}
+</style>
